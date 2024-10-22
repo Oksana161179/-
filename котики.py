2e6073fdca2f882ab1a03adc9f08985e37f370d0
Оксана <oksana.fomenko.161979@gmail.com>
2024-10-22 16:17:15 +0600

    Создана функция для получения изображения с котиком

--- котики.py
+++ котики.py
@@ -1,11 +1,24 @@
-from cProfile import label
 from tkinter import *
 from PIL import Image, ImageTk#работа с изображениями
 import requests
-from io import BytesIO#io-работа с вводом, выводом информации. BytesIO-работа с байтами
+from io import BytesIO #io-работа с вводом, выводом информации. BytesIO-работа с байтами
 
-from Scripts.графический_редактор_с_метками_для_изменения_цвета_и_загрузкой_изображений_из_файла import load_image
-from выводим_список_папок_в_консоль import window
+from pygame.examples.cursors import image
+
+
+def load_image():#создаем функцию загрузки изображений
+    try:#обрабатываем исключения
+        response = requests.get(url)#получаем ответ-response,
+        # после того как сделали запрос-requests.get по ссылке-url
+        response.raise_for_status()#строчка нужна для обработки исключений.
+        # если будет какая-то ошибка, то сдесь мы ее и получим
+        image_data = BytesIO(response.content)#делаем изображение:
+        # в переменную-image_data положим обработанное изображение
+        img = Image.open(image_data)#делаем изображение нормальной картинкой
+        return ImageTk.PhotoImage(img)#функция вернет картинку-img
+    except Exception as e:#если ошибка
+        print(f"Произошла ошибка: {e}")
+        return None#если произошла ошибка, то функция ничего не вернет
 
 window = Tk()#создаем окно
 window.title("Cats!")#заголовок окна
